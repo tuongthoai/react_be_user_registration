faf2f28a99d1beddf33723eaa64e0fbea0bdfbcf
Tăng Tường Thoại <thoaitang24603@gmail.com>
2024-11-05 20:19:12 +0700

    setting cors

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './all-exceptions.filter';
 
 async function bootstrap() {
+  const corsOptions = {
+    origin: 'https://react-fe-user-registration.onrender.com',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  };
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors(corsOptions);
   app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
